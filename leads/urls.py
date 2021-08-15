1fc764ef73181996bf160cdf0cd5e4cb16125b46
mehrozmalik <mehrozmalik7@gmail.com>
2021-08-15 18:16:48 +0500

    Adding update html page

--- leads/urls.py
+++ leads/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import lead_create, lead_list, lead_detail
+from .views import lead_create, lead_list, lead_detail, lead_update
 
 
 app_name = "leads"
@@ -7,5 +7,6 @@ app_name = "leads"
 urlpatterns = [
     path('', lead_list),
     path('create/', lead_create),
+    path('<int:pk>/update/', lead_update),
     path('<int:pk>/', lead_detail),
 ]
